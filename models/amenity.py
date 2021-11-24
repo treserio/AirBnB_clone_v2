2c9646ae79c7209245c1fbedd2355af204e843de
Tres Serio <treserio@hotmail.com>
2021-11-24 13:27:08 -0600

    +docstr on t_b_m

--- models/amenity.py
+++ models/amenity.py
@@ -2,12 +2,9 @@
 """ Amenity Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship, backref
 
 
 class Amenity(BaseModel, Base):
     """The Amenity Class"""
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
-#    place_amenities = relationship("Place",
-#                                   secondary='place_amenity')
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" """
+"""Module for testing BaseModel Class """
 from models.base_model import BaseModel
 import unittest
 import datetime
@@ -9,38 +9,39 @@ import os
 
 
 class test_basemodel(unittest.TestCase):
-    """ """
+    """BaseModel testing"""
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """Init for basemodel testing"""
         super().__init__(*args, **kwargs)
         self.name = 'BaseModel'
         self.value = BaseModel
 
     def setUp(self):
-        """ """
+        """pre-test actions"""
         pass
 
     def tearDown(self):
+        """post test actions"""
         try:
             os.remove('file.json')
         except Exception:
             pass
 
     def test_default(self):
-        """ """
+        """type of .value()"""
         i = self.value()
         self.assertEqual(type(i), self.value)
 
     def test_kwargs(self):
-        """ """
+        """kwargs creation with a copy"""
         i = self.value()
         copy = i.to_dict()
         new = BaseModel(**copy)
         self.assertFalse(new is i)
 
     def test_kwargs_int(self):
-        """ """
+        """try creating with incorrect kwargs"""
         i = self.value()
         copy = i.to_dict()
         copy.update({1: 2})
@@ -48,7 +49,7 @@ class test_basemodel(unittest.TestCase):
             new = BaseModel(**copy)
 
     def test_save(self):
-        """ Testing save """
+        """ Testing save"""
         i = self.value()
         i.save()
         key = self.name + "." + i.id
@@ -57,41 +58,41 @@ class test_basemodel(unittest.TestCase):
             self.assertEqual(j[key], i.to_dict())
 
     def test_str(self):
-        """ """
+        """str method"""
         i = self.value()
         self.assertEqual(str(i), '[{}] ({}) {}'.format(self.name, i.id,
                          i.__dict__))
 
     def test_todict(self):
-        """ """
+        """to_dict method"""
         i = self.value()
         n = i.to_dict()
         self.assertEqual(i.to_dict(), n)
 
     def test_kwargs_none(self):
-        """ """
+        """test creation with None for Kwarg key:value"""
         n = {None: None}
         with self.assertRaises(TypeError):
             new = self.value(**n)
 
     def test_kwargs_one(self):
-        """ """
+        """kwargs with one value, prev failed"""
         n = {'Name': 'test'}
-        with self.assertRaises(KeyError):
-            new = self.value(**n)
+        new = self.value(**n)
+        self.assertIsInstance(new, BaseModel)
 
     def test_id(self):
-        """ """
+        """ confirm id is a str"""
         new = self.value()
         self.assertEqual(type(new.id), str)
 
     def test_created_at(self):
-        """ """
+        """conform created_at is datetime"""
         new = self.value()
         self.assertEqual(type(new.created_at), datetime.datetime)
 
     def test_updated_at(self):
-        """ """
+        """confirm updated_at is datetime & is updated when needed"""
         new = self.value()
         self.assertEqual(type(new.updated_at), datetime.datetime)
         n = new.to_dict()
