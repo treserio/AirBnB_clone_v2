b8c305d19854a8e54c982834da2d51c40355926f
Tres Serio <treserio@hotmail.com>
2021-12-14 16:59:12 -0600

    5.1 refactor strict_slashes

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -5,29 +5,30 @@ if __name__ == '__main__':
     from flask import render_template
 
     app = Flask(__name__)
+    app.url_map.strict_slashes = False
 
-    @app.route('/', strict_slashes=False)
+    @app.route('/')
     def s():
         return 'Hello HBNB!'
 
-    @app.route('/hbnb', strict_slashes=False)
+    @app.route('/hbnb')
     def s_hbnb():
         return 'HBNB'
 
-    @app.route('/c/<text>', strict_slashes=False)
+    @app.route('/c/<text>')
     def s_c_text(text):
         return 'C {}'.format(text.replace('_', ' '))
 
-    @app.route('/python/', strict_slashes=False)
-    @app.route('/python/<text>', strict_slashes=False)
+    @app.route('/python/')
+    @app.route('/python/<text>')
     def s_python_text(text='is cool'):
         return 'Python {}'.format(text.replace('_', ' '))
 
-    @app.route('/number/<int:n>', strict_slashes=False)
+    @app.route('/number/<int:n>')
     def s_number_n(n):
         return '{} is a number'.format(n)
 
-    @app.route('/number_template/<int:n>', strict_slashes=False)
+    @app.route('/number_template/<int:n>')
     def s_numberTemplate_n(n):
         return render_template('5-number.html', n=n)
 
