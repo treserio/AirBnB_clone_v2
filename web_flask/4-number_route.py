2a3c57908e546dfb3f61991f7a68840b284adfec
Tres Serio <treserio@hotmail.com>
2021-12-14 16:50:34 -0600

    4.4 refactor

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -26,13 +26,9 @@ def s_python_text(text='is cool'):
     return 'Python {}'.format(text.replace('_', ' '))
 
 
-@app.route('/number/<n>', strict_slashes=False)
+@app.route('/number/<int:n>', strict_slashes=False)
 def s_number_n(n):
-    try:
-        int(n)
-        return '{} is a number'.format(n)
-    except Exception:
-        pass
+    return '{} is a number'.format(n)
 
 
 if __name__ == '__main__':
