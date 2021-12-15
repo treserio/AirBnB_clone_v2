62bb7eeabd0dff3116a679c2befca234e2b86ed5
Tres Serio <treserio@hotmail.com>
2021-12-14 19:40:51 -0600

    8.2 more specific imports

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,19 +3,20 @@
 
 
 if __name__ == '__main__':
-    import flask
-    import models
+    from flask import Flask, render_template
+    from models import storage
+    from models.state import State
 
-    app = flask.Flask(__name__)
+    app = Flask(__name__)
     app.url_map.strict_slashes = False
 
     @app.teardown_appcontext
     def teardown(context):
-        models.storage.close()
+        storage.close()
 
     @app.route('/states_list')
     def statesList():
-        ls = models.storage.all(models.State)
-        return flask.render_template('7-states_list.html', States=ls)
+        ls = storage.all(State)
+        return render_template('7-states_list.html', States=ls)
 
     app.run(host='0.0.0.0', port='5000')
