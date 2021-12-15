1cdb927195482b3211a7f3d76995be7fdbded202
Tres Serio <treserio@hotmail.com>
2021-12-15 16:06:41 -0600

    11 init

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''Flask app using filestorage'''
+
+
+if __name__ == '__main__':
+    from flask import Flask, render_template
+    import models
+
+    app = Flask(__name__)
+    app.url_map.strict_slashes = False
+
+    @app.teardown_appcontext
+    def teardown(context):
+        models.storage.close()
+
+    @app.route('/hbnb_filters')
+    def s_hbnbFilters():
+        return render_template(
+            '10-hnb_filters.html',
+            st=models.storage.all(models.State),
+            amnty=models.storage.all(models.Amenity)
+        )
+
+    app.run(host='0.0.0.0', port='5000')
